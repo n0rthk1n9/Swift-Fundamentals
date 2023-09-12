f3a380537afd461dcefafc0b523d7dbac89d3f49
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-12 14:45:04 +0200

    learn about playgrounds

--- core_concepts.playground/Pages/02 - Xcode Playgrounds & Comments.xcplaygroundpage/Contents.swift
+++ core_concepts.playground/Pages/02 - Xcode Playgrounds & Comments.xcplaygroundpage/Contents.swift
@@ -2,6 +2,15 @@
 //: ## Episode 02: Xcode Playgrounds
 import Foundation
 
-var str = "Hello, playground"
+var welcomeMessage = "Hello, playground"
+let goodbyeMessage = "See you soon!"
+
+// The code below prints out a goodby message
+// Yet another comment
+
+/* This is a comment
+ spread out over a few lines
+ also, a kaiku */
+print(goodbyeMessage)
 
 //: [⇒ Next: 03 - Booleans & Comparison Operators](@next)
