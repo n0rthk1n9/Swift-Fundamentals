308053cc1df9627fb8c8e65fbae29653faa40146
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-17 19:53:13 +0200

    learn sets

--- 04_more_collections.playground/Pages/05 - Working with Sets.xcplaygroundpage/Contents.swift
+++ 04_more_collections.playground/Pages/05 - Working with Sets.xcplaygroundpage/Contents.swift
@@ -1,5 +1,20 @@
 //: [⇐ Previous: 04 - Challenge - Dictionaries](@previous)
 //: ## Episode 05: Creating and Populating Sets
-
+var someSet: Set<Int> = [1, 2, 3, 1]
+// let someArray: Array<Int>
+// let someDictionary: Dictionary<String, Int>
+someSet.contains(1)
+someSet.contains(99)
+someSet.insert(5)
+let removedElement = someSet.remove(3)
+let nilElement = someSet.remove(42)
+print(someSet)
+let anotherSet: Set<Int> = [5, 7, 13]
+let intersection = someSet.intersection(anotherSet)
+let difference = someSet.symmetricDifference(anotherSet)
+let union = someSet.union(anotherSet)
+someSet.formUnion(anotherSet)
+print(someSet)
+print(anotherSet)
 
 //: [⇒ Next: 06 - Challenge - Sets](@next)
