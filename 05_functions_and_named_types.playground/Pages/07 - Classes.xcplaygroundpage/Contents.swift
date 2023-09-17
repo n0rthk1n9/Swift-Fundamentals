a29a2429824b155315291747a093b892cc1b7d7d
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-17 21:10:41 +0200

    learn classes

--- 05_functions_and_named_types.playground/Pages/07 - Classes.xcplaygroundpage/Contents.swift
+++ 05_functions_and_named_types.playground/Pages/07 - Classes.xcplaygroundpage/Contents.swift
@@ -2,11 +2,42 @@
 //: ## Episode 07: Classes
 
 // -----------------------------------
-//struct Actor {
-//  let name: String
-//  var filmography: [String] = []
-//}
+// struct Actor {
+//    let name: String
+//    var filmography: [String] = []
+//
+//    mutating func signOnForSequel(franchiseName: String) {
+//        filmography.append("\(franchiseName) sequel")
+//    }
+// }
+
+class Actor {
+    let name: String
+    var filmography: [String] = []
+
+    init(name: String, filmography: [String]) {
+        self.name = name
+        self.filmography = filmography
+    }
+
+    func signOnForSequel(franchiseName: String) {
+        filmography.append("\(franchiseName) sequel")
+    }
+}
+
 // -----------------------------------
 
+let gotgStar = Actor(name: "Zoe Salanda", filmography: ["Guardian of the Galaxy"])
+gotgStar.filmography.append("Avatar")
+let starTrackStar = gotgStar
+starTrackStar.filmography.append("Star Treck")
+var avatarStar = starTrackStar
+for franchiseName in avatarStar.filmography {
+    avatarStar.signOnForSequel(franchiseName: franchiseName)
+}
+
+print(avatarStar.filmography)
+print(starTrackStar.filmography)
+print(gotgStar.filmography)
 
 //: [⇒ Next: 08 - Challenge - Classes](@next)
