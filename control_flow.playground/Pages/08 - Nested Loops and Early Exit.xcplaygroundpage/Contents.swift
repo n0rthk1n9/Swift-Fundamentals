011cf4d9ba4100fe0435c9f03298b048ab96c832
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-17 18:57:12 +0200

    learn nested loops and early exits

--- control_flow.playground/Pages/08 - Nested Loops and Early Exit.xcplaygroundpage/Contents.swift
+++ control_flow.playground/Pages/08 - Nested Loops and Early Exit.xcplaygroundpage/Contents.swift
@@ -5,6 +5,38 @@ let daysOfTheWeek: [String] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thur
 
 let poolTemperature: [Int] = [78, 81, 74, 80, 79, 83, 84]
 
+for i in 0 ..< daysOfTheWeek.count {
+    if daysOfTheWeek[i] == "Thursday" {
+        break
+    }
+    print("\(daysOfTheWeek[i]): \(poolTemperature[i])")
+}
 
+print("----")
+
+for i in 0 ..< daysOfTheWeek.count {
+    if daysOfTheWeek[i] == "Thursday" {
+        print("I'm in love")
+        continue
+    }
+    print("\(daysOfTheWeek[i]): \(poolTemperature[i])")
+}
+
+print("----")
+
+floor_loop: for floor in 11 ... 15 {
+    if floor == 13 {
+        continue
+    }
+    room_loop: for room in 1 ... 4 {
+        if room == 1 {
+            continue
+        }
+        if floor == 12, room == 3 {
+            continue floor_loop
+        }
+        print("Floor: \(floor), Room: \(room)")
+    }
+}
 
 //: [⇒ Next: 09 - Challenge - Nested Loops and Early Exit](@next)
