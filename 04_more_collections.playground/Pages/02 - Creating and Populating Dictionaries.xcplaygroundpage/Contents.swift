5fc4e8a56a032cc75ab5ceb10441a0e80e1ef7c4
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-17 19:25:24 +0200

    learn creating and populating dictionaries

--- 04_more_collections.playground/Pages/02 - Creating and Populating Dictionaries.xcplaygroundpage/Contents.swift
+++ 04_more_collections.playground/Pages/02 - Creating and Populating Dictionaries.xcplaygroundpage/Contents.swift
@@ -1,5 +1,15 @@
 //: [⇐ Previous: 01 - Introduction](@previous)
 //: ## Episode 02: Creating and Populating Dictionaries
+var emptyDictionary: [String: Int] = [:]
 
-
+var nameAndPets = ["Ron": "Rat",
+                   "Rincewind": "Luggage",
+                   "Thor": "Hammer",
+                   "Goku": "Flying Nimbus"]
+print(nameAndPets)
+nameAndPets.updateValue("Flecki", forKey: "Jan")
+nameAndPets["Maike"] = "Ossi"
+nameAndPets.updateValue("Owl", forKey: "Ron")
+nameAndPets["Ron"] = "🦉 Owl"
+print(nameAndPets)
 //: [⇒ Next: 03 - Accessing and Working with Dictionaries](@next)
