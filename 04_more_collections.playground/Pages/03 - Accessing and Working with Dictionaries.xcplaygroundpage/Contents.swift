9cfcb0e69e5e174ca1cb3fb2b864b7b9dcb9d079
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-17 19:36:14 +0200

    learn accessing and working with  dictionaries

--- 04_more_collections.playground/Pages/03 - Accessing and Working with Dictionaries.xcplaygroundpage/Contents.swift
+++ 04_more_collections.playground/Pages/03 - Accessing and Working with Dictionaries.xcplaygroundpage/Contents.swift
@@ -2,15 +2,41 @@
 //: ## Episode 03: Accessing and Working with Dictionaries
 
 var namesAndPets = [
-    "Ron" : "🦉 Owl",
-    "Rincewind" : "🛄 Luggage",
-    "Thor" : "🔨 Hammer",
-    "Goku" : "☁️ Flying Nimbus",
-    "Chris" : "🐶 Mango",
-    "Calvin" : "🐯 Tiger",
-    "Hiccup" : "🐉 Toothless"
+    "Ron": "🦉 Owl",
+    "Rincewind": "🛄 Luggage",
+    "Thor": "🔨 Hammer",
+    "Goku": "☁️ Flying Nimbus",
+    "Chris": "🐶 Mango",
+    "Calvin": "🐯 Tiger",
+    "Hiccup": "🐉 Toothless"
 ]
 
+namesAndPets["Rincewind"]
+namesAndPets["Jan"]
+let janPet = namesAndPets["Jan"] ?? "No Flecki for Jan"
+namesAndPets.isEmpty
+namesAndPets.count
+namesAndPets.removeValue(forKey: "Goku")
+namesAndPets["Hicup"] = nil
+print(namesAndPets)
+for (character, pet) in namesAndPets {
+    print("\(character) has a \(pet)")
+}
 
+for (name, _) in namesAndPets {
+    print(name)
+}
+
+print("----")
+
+for name in namesAndPets.keys {
+    print(name)
+}
+
+print("----")
+
+for pet in namesAndPets.values {
+    print(pet)
+}
 
 //: [⇒ Next: 04 - Challenge - Dictionaries](@next)
